43ce237ae87eef61963c854bb698d8070ad1200f
Khaled S Momani <khaledmomani4@gmail.com>
2021-01-26 02:20:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,8 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |list| 
-    list.include?("#{cheese_types}")
+    list.include?(cheese_types)
   end
 end
+
+
