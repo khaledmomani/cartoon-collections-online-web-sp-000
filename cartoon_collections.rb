44d46e7006cd2ce62ea4c61f0c0fcdd677aeca67
Khaled S Momani <khaledmomani4@gmail.com>
2021-01-26 02:25:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,9 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  cheese = ["cheddar", "gouda", "camembert"]
+  cheese = [cheddar, gouda, camembert]
   array.find do |list| 
-    list.include?(cheese)
+    list.include?("#{cheese}")
   end
 end
 
